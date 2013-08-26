81fd6c5e93291fa06b7baced287fb1d559a99fd0
Erdong Feng <edfeng@thoughtworks.com>
2013-08-26 13:35:15 +0800

    baseline.

--- src/main/java/com/github/fedcuit/datatank/controller/CallbackController.java
+++ src/main/java/com/github/fedcuit/datatank/controller/CallbackController.java
@@ -1,5 +1,7 @@
 package com.github.fedcuit.datatank.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +12,13 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class CallbackController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallbackController.class);
 
     @RequestMapping(value = "/jinshuju/callback", method = POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public
     @ResponseBody
     String callback(@RequestBody String formData) {
+        LOGGER.info("data pushed from jinshuju" + formData);
         return formData;
     }
 }
