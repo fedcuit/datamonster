8237ceda0bacac0bd9399febca3c0619c95a470e
Erdong Feng <edfeng@thoughtworks.com>
2013-08-27 10:12:04 +0800

    Test for dump form data to file.

--- src/main/java/com/github/fedcuit/datatank/controller/CallbackController.java
+++ src/main/java/com/github/fedcuit/datatank/controller/CallbackController.java
@@ -1,7 +1,5 @@
 package com.github.fedcuit.datatank.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +10,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class CallbackController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CallbackController.class);
-
     @RequestMapping(value = "/jinshuju/callback", method = POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public
     @ResponseBody
     String callback(@RequestBody String formData) {
-        LOGGER.info("data pushed from jinshuju" + formData);
         return formData;
     }
 }
---
+++ src/test/java/com/github/fedcuit/datatank/controller/CallbackControllerTest.groovy
@@ -0,0 +1,35 @@
+package com.github.fedcuit.datatank.controller
+
+import org.junit.Ignore
+import org.junit.Test
+
+import static org.fest.assertions.api.Assertions.assertThat
+
+
+class CallbackControllerTest {
+
+    @Test
+    @Ignore
+    void shouldDumpFormDataToFile() {
+        def formData = '''
+            {
+              "form": "xd0gLh",
+              "entry": {
+                "serial_number": 123,
+                "field_5": "选项1",
+                "field_6": "选项1",
+                "field_11": "这是一段文字",
+                "creator_name": "这是一行文字",
+                "created_at": "2013-08-26T08:58:00Z",
+                "updated_at": "2013-08-26T08:58:00Z"
+              }
+            }
+            '''
+        def controller = new CallbackController()
+        controller.callback(formData)
+
+        def dumpedData = this.getClass().getResourceAsStream('/application.json').text
+        assertThat(dumpedData).isEqualTo(formData)
+    }
+
+}
